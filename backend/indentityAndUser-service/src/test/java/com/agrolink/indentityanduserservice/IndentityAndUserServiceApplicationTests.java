4077333de84e26ac154eb94733e09925872c0071
RCN <ravindunishshankawork@gmail.com>
2025-11-28 12:02:11 +0530

    Configure the project foundation

---
+++ backend/indentityAndUser-service/src/test/java/com/agrolink/indentityanduserservice/IndentityAndUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrolink.indentityanduserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndentityAndUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
