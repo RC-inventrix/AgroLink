14054c53b2ece178221e98c551daba685396f8c2
maleesha <maleeshadilshi111@gmail.com>
2025-12-08 10:19:51 +0530

    Implement secure admin login

---
+++ backend/indentityAndUser-service/src/main/java/com/agrolink/indentityanduserservice/model/Admin.java
@@ -0,0 +1,25 @@
+package com.agrolink.indentityanduserservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "admins")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
