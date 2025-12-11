85501fc45bb5efdea27effe93fc13b4179f3fccd
RCN <ravindunishshankawork@gmail.com>
2025-12-11 20:17:13 +0530

    added the order and payement microservice

---
+++ backend/orderPayment-service/src/main/java/com/agrolink/orderpaymentservice/OrderPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agrolink.orderpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ backend/orderPayment-service/src/test/java/com/agrolink/orderpaymentservice/OrderPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrolink.orderpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
