8f927e1fa7d10f82152576579988a8865821d125
RCN <ravindunishshankawork@gmail.com>
2026-01-04 23:27:34 +0530

    order service + cart

--- backend/orderPayment-service/src/main/java/com/agrolink/orderpaymentservice/repository/CartRepository.java
+++ backend/orderPayment-service/src/main/java/com/agrolink/orderpaymentservice/repository/CartRepository.java
@@ -1,4 +1,11 @@
 package com.agrolink.orderpaymentservice.repository;
 
-public class CartRepository {
+import com.agrolink.orderpaymentservice.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface CartRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByUserId(Long userId);
+    Optional<CartItem> findByUserIdAndProductId(Long userId, Long productId);
 }
