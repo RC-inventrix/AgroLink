475261ad0e75102fb4d2fc03971e615f2a2f3455
RCN <ravindunishshankawork@gmail.com>
2026-03-04 01:36:25 +0530

    feat(crop Recommend):Connected the backend with the frontend

--- backend/productCatalog-service/src/main/java/com/agrolink/productcatalogservice/controller/CropController.java
+++ backend/productCatalog-service/src/main/java/com/agrolink/productcatalogservice/controller/CropController.java
@@ -1,12 +1,13 @@
 package com.agrolink.productcatalogservice.controller;
 
+import com.agrolink.productcatalogservice.dto.CropRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/crop")
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class CropController {
 
     @Autowired
